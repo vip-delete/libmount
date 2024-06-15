951ba3612116d0421bc03d850e715f236159fcdd
vip-delete <vip.delete@gmail.com>
2024-06-15 17:44:13 +0200

    add tests

--- src/test/javascript/libmount/mount.test.js
+++ src/test/javascript/libmount/mount.test.js
@@ -6,11 +6,29 @@ const buf = readFileSync("src/test/resources/freedos722.img", { flag: "r" });
 
 test("mount", () => {
   const fs = LibMount.mount(buf.buffer);
+  const root = fs.getRoot();
+  expect(root.getName()).toBe("");
+  expect(root.getAbsolutePath()).toBe("/");
+
   const kernel = fs.getFile("kernel.sys");
+  expect(kernel.getName()).toBe("KERNEL.SYS");
   expect(kernel.getAbsolutePath()).toBe("/KERNEL.SYS");
 
-  const minesweeper = fs.getFile("/games/minesw~1.com");
+  const games = fs.getFile("GAMES");
+  expect(games.getName()).toBe("games");
+  expect(games.getAbsolutePath()).toBe("/games");
+
+  const games1 = fs.getFile("/GAMES");
+  expect(games1.getName()).toBe("games");
+  expect(games1.getAbsolutePath()).toBe("/games");
+
+  const games2 = fs.getFile("/GaMeS");
+  expect(games2.getName()).toBe("games");
+  expect(games2.getAbsolutePath()).toBe("/games");
+
+  const minesweeper = fs.getFile("/games/MiNeSw~1.COM");
   expect(minesweeper.getName()).toBe("minesweeper.com");
+  expect(minesweeper.getAbsolutePath()).toBe("/games/minesweeper.com");
 
   const files = fs.listFiles(fs.getRoot());
   expect(files.length).toBe(22);

36c3a81af07f8e882424216d6aec59648146637e
vip-delete <vip.delete@gmail.com>
2025-11-11 13:11:59 +0100

    remove serve-static

--- scripts/server.mjs
+++ scripts/server.mjs
@@ -1,13 +1,27 @@
 import express from "express";
-import serveStatic from "serve-static";
+import Path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
+
+/**
+ * @param {string} rel
+ * @returns {string}
+ */
+export const abs = (rel) => Path.resolve(Path.dirname(fileURLToPath(import.meta.url)), "../" + rel);
 
 app.use("/.gz$/", (req, res, next) => {
   res.setHeader("Content-Encoding", "gzip");
   next();
 });
 
-app.use(serveStatic("."));
+["src", "static", "images"].forEach((dir) => {
+  app.use("/" + dir, express.static(abs(dir)));
+});
+
+app.get(["/", "/index.html"], (req, res) => {
+  res.sendFile(abs("index.html"));
+});
 
 app.listen(3000);
 console.log("Listening \x1b[92mhttp://localhost:3000\x1b[0m");
